6c9f09f1c29f4975e5b6b888ec385d044e8c0922
Brandon Huang <hotbrandon@hotmail.com>
2026-05-30 15:10:28 +0800

    done with the branch

--- main.go
+++ main.go
@@ -38,23 +38,11 @@ func FilterWordsByLength(file *os.File, minLen, maxLen int) []string {
 	return words
 }
 
-func GetRandomWord(words []string) string {
-	return words[rand.IntN(len(words))]
-}
-
-// display the first and last alphabet of a word, and hide others with a '_'
-func GetInitialWord(word string) string {
-	runes := []rune(word)
-	length := len(runes)
-	if length <= 2 {
-		return word
+func GetRandomWord(words []string) (string, error) {
+	if len(words) == 0 {
+		return "", fmt.Errorf("word list is empty")
 	}
-
-	for i := 1; i < length-1; i++ {
-		runes[i] = '_'
-	}
-
-	return string(runes)
+	return words[rand.IntN(len(words))], nil
 }
 
 func CheckValidAlphabet(c rune) bool {
@@ -66,81 +54,124 @@ func findLetterPosition(letter rune, word string) []int {
 	for idx, char := range word {
 		if letter == char {
 			indexes = append(indexes, idx)
-			fmt.Printf("found at index %d, letter %c\n", idx, char)
+			// fmt.Printf("found at index %d, letter %c\n", idx, char)
 		}
 	}
 	return indexes
 }
 
-func fillinLetter(guessedWord string, letter rune, positions []int) string {
-	runes := []rune(guessedWord)
-	for _, pos := range positions {
-		runes[pos] = letter
+// maskLetters  is used to mask the word with '_' except the first and last letter, and return the masked word.
+func maskLetters(word string) string {
+	runes := []rune(word)
+	length := len(runes)
+
+	// we may not need this, because a word of 2 letters isn't worth playing.
+	if length <= 2 {
+		return word
 	}
+
+	for i := 1; i < length-1; i++ {
+		runes[i] = '_'
+	}
+
 	return string(runes)
 }
 
-func main() {
-	file, err := OpenDictionary("words_alpha.txt")
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
+func unmaskLetters(word string, letter rune, positions []int) string {
+	runes := []rune(word)
 
-	word_list := FilterWordsByLength(file, 3, 10)
+	for _, pos := range positions {
+		runes[pos] = letter
+	}
+
+	return string(runes)
+}
+
+func playGame(wordList []string) {
 	for {
-		chosenWord := GetRandomWord(word_list)
-		maxGuessCount := len(chosenWord)
-		fmt.Printf("max try %d, enter a letter or 'quit' to exit.\n", maxGuessCount)
-		guessed_word := GetInitialWord(chosenWord)
-		guessed_letters := make(map[rune]bool)
+		randomWord, err := GetRandomWord(wordList)
+		if err != nil {
+			fmt.Println("Error occurred while fetching a random word:", err)
+			return
+		}
+		const bonusTry = 2
+		maxGuessCount := len(randomWord) + bonusTry
+
+		fmt.Printf("%d letters, max try %d, enter a letter or 'quit' to exit.\n\n", len(randomWord), maxGuessCount)
+		guessedWord := maskLetters(randomWord)
+		guessedLetters := make(map[rune]bool)
+		reader := bufio.NewReader(os.Stdin)
 
 		for i := 0; i < maxGuessCount; i++ {
-			fmt.Printf("word: %s\n", guessed_word)
-			fmt.Printf("you have %d tries left.\n", maxGuessCount-i)
-			fmt.Println("Guess the next letter:")
-			reader := bufio.NewReader(os.Stdin)
+			// print current word
+			fmt.Printf("word: %s\n", guessedWord)
 
+			// ask for input
+			fmt.Printf("You have %d tries left. guess a letter => ", maxGuessCount-i)
+
+			// read input
 			input, _ := reader.ReadString('\n')
+
+			// validate input
 			trimmedInput := strings.TrimSpace(input)
 
-			if trimmedInput == "quit" {
-				fmt.Println("good bye!")
-				os.Exit(0)
-			}
 			if len(trimmedInput) == 0 {
 				fmt.Println("invalid alphabet, please use 'A' - 'Z' or 'a' - 'z'")
 				continue
+			}
+
+			// compare input
+			if trimmedInput == "quit" {
+				return
 			}
 
 			if !CheckValidAlphabet(rune(trimmedInput[0])) {
 				fmt.Println("invalid alphabet, please use 'A' - 'Z' or 'a' - 'z'")
 				continue
 			}
-			guessed_letter := rune(trimmedInput[0])
-			if guessed_letters[guessed_letter] {
-				fmt.Printf("you have already guessed the letter %c.\n", guessed_letter)
+
+			guessedLetter := rune(trimmedInput[0])
+			if guessedLetters[guessedLetter] {
+				fmt.Printf("you have already guessed the letter %c.\n", guessedLetter)
+				continue
 			}
-			guessed_letters[guessed_letter] = true
 
-			// find the letter in chosenWord, and return the position(s).
-			positions := findLetterPosition(rune(trimmedInput[0]), chosenWord)
-			guessed_word = fillinLetter(guessed_word, rune(trimmedInput[0]), positions)
+			// push the new letter into guessed letters slice.
+			guessedLetters[guessedLetter] = true
 
-			if guessed_word == chosenWord {
-				fmt.Printf("Congratulations! You guessed the word: %s\n\n", chosenWord)
+			// find the letter in randomWord, and return the position(s).
+			positions := findLetterPosition(rune(trimmedInput[0]), randomWord)
+			guessedWord = unmaskLetters(guessedWord, rune(trimmedInput[0]), positions)
+
+			// compare with the final answer
+			if guessedWord == randomWord {
+				fmt.Printf("Congratulations! You guessed the word: %s\n\n", randomWord)
 				break
 			}
 
-			fmt.Println("guessed letters: ")
-			for c := range guessed_letters {
+			fmt.Print("guessed letters => ")
+			for c := range guessedLetters {
 				fmt.Printf("%c ", c)
 			}
 			fmt.Println()
+			fmt.Println()
+
 			if i == maxGuessCount-1 {
-				fmt.Printf("Game over! The word was: %s\n\n", chosenWord)
+				fmt.Printf("Game over! The word was: %s\n\n", randomWord)
 			}
 		}
 	}
+}
+
+func main() {
+	file, err := OpenDictionary("wordlist.10000.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	wordList := FilterWordsByLength(file, 3, 10)
+	playGame(wordList)
+	fmt.Println("good bye!")
 
 }
